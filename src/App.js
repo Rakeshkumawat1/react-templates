7807bc63e7d1aa212dbdf3783c279ea05e057226
Rakesh kumawat <rakesh.k@sarv.com>
2023-07-05 17:00:59 +0530

    Feat: add dashboard route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-
 import { Routes, Route } from "react-router-dom";
-import Login from './pages/Auth/Login';
 
 //All pages
 import LandingPage from './pages/landingPage';
+import Home from './pages/Home';
+import Login from './pages/Auth/Login';
+
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
     <Routes>
       <Route path='/' element={<LandingPage/>} />
       <Route path='/login' element={<Login/>} />
+      <Route path='/dashboard' element={<Home/>} />
         {/* <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="blogs" element={<Blogs />} />
