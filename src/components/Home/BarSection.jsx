8988ef8e6f33265d0cb33643a3795f6506f325f5
Rakesh kumawat <rakesh.k@sarv.com>
2023-07-06 16:51:43 +0530

    Feat: code structure optimization

--- src/components/Home/BarSection.jsx
+++ src/components/Home/BarSection.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import {
-  Box, Toolbar, List, CssBaseline, Typography, IconButton
-  , ListItem, ListItemButton, ListItemIcon, ListItemText,
-} from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { List, ListItem, ListItemButton, ListItemIcon, ListItemText, } from '@mui/material';
 import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import MailIcon from '@mui/icons-material/Mail';
 import { imgUrl } from '../../config';
 
-import KeyboardCommandKeyIcon from '@mui/icons-material/KeyboardCommandKey';
-import SearchIcon from '@mui/icons-material/Search';
+// Icons
 import HomeIcon from '@mui/icons-material/Home';
+import MailIcon from '@mui/icons-material/Mail';
 
+// Define drawer width length.
 const drawerWidth = 220;
 
+// Drawer open handler css.
 const openedMixin = (theme) => ({
   width: drawerWidth,
   transition: theme.transitions.create('width', {
@@ -27,6 +22,7 @@ const openedMixin = (theme) => ({
   borderRight: '1px dashed rgba(0, 0, 0, 0.12)',
 });
 
+// Drawer close handler css.
 const closedMixin = (theme) => ({
   transition: theme.transitions.create('width', {
     easing: theme.transitions.easing.sharp,
@@ -40,6 +36,7 @@ const closedMixin = (theme) => ({
   borderRight: '1px dashed rgba(0, 0, 0, 0.12)',
 });
 
+// Drawer header style hadler
 const DrawerHeader = styled('div')(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
@@ -49,32 +46,8 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
 
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-  ...(!open && {
-    left: '81px',
-    width: `calc(100% - 81px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  })
-}));
 
+// Drawer style handler
 const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
   ({ theme, open }) => ({
     width: drawerWidth,
@@ -92,94 +65,42 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
   }),
 );
 
-export default function BarSection() {
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+export default function BarSection({ open }) {
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <IconButton onClick={()=> open ? handleDrawerClose() : handleDrawerOpen()} sx={{
-        position: 'absolute',
-        top: '18px',
-        left: open ? '205px' : '65px',
-        zIndex: 99999,
-        border: '1px dashed rgba(0, 0, 0, 0.12)',
-        width: '30px',
-        height: '30px',
-        backgroundColor: '#f5f6f7'
-      }}>
-        {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-      </IconButton>
-      <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-          >
-            <SearchIcon />
-          </IconButton>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-          // sx={{
-          //   marginRight: 5,
-          //   ...(open && { display: 'none' }),
-          // }}
-          >
-            <KeyboardCommandKeyIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            K
-          </Typography>
-        </Toolbar>
-      </AppBar>
+    <Drawer variant="permanent" open={open}>
 
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <img src={imgUrl?.navLogo} alt="" style={{ width: '40px', height: '40px' }} />
-        </DrawerHeader>
-        <List>
-          {['Home', 'Cards', 'Users', 'Analytics'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
+      {/* Drawer header section. */}
+      <DrawerHeader>
+        <img src={imgUrl?.navLogo} alt="" style={{ width: '40px', height: '40px' }} />
+      </DrawerHeader>
+
+      {/* Side bar list section. */}
+      <List>
+        {['Home', 'Cards', 'Users', 'Analytics'].map((text, index) => (
+          <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+            <ListItemButton
+              sx={{
+                minHeight: 48,
+                justifyContent: open ? 'initial' : 'center',
+                flexDirection: open ? 'row' : 'column',
+                px: 3.5,
+              }}
+            >
+              <ListItemIcon
                 sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  flexDirection: open ? 'row' : 'column',
-                  px: 3.5,
+                  minWidth: 0,
+                  mr: open ? 2 : 'auto',
+                  justifyContent: 'center',
                 }}
               >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 2 : 'auto',
-                    justifyContent: 'center',
-                  }}
-                >
-                  {index % 2 === 0 ? <HomeIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} primaryTypographyProps={{ fontSize: open ? '1rem' : '12px' }} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        {/* <DrawerHeader /> */}
-      </Box>
-    </Box>
+                {index % 2 === 0 ? <HomeIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} primaryTypographyProps={{ fontSize: open ? '1rem' : '12px' }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
   );
 }
